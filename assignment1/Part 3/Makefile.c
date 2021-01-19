365e223930b02358e72d77fec972fddb3f810b2a
btphill4 <btphill4@asu.edu>
2021-01-19 11:14:07 -0800

    Assignment 1 changes

---
+++ assignment1/Part 3/Makefile.c
@@ -0,0 +1,10 @@
+all: c_program
+
+c_program: c_program.c 
+	$(CC) c_program.c -o c_program
+
+c_program.o: c_program.c
+	$(CC) $(CFLAGS) c_program.c
+    
+clean:
+	rm -rf *o c_program 
---
+++ assignment1/Part 4/command.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** args)
+{
+    string sTxt = "Testing string so yuh";
+   /* while(cin >> sTxt)
+    {
+        getline(cin,sTxt);
+    }*/
+    
+    int sSize = sTxt.length(); 
+
+    char* cTxt = new char[sSize];
+        for(int i = 0; i < sSize; i++)
+        {
+            cTxt[i] = sTxt[i];
+        }
+    
+    for(int i = 0; i < sSize; i++)                    //iterate the array starting with the max size 1 less than cTxt
+    {
+    cout << cTxt[i] << endl;
+    }
+
+   return 0;
+}
