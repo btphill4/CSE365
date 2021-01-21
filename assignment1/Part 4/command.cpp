ca863d932c93de7a45f591b6356e1f41316d1e40
btphill4 <btphill4@asu.edu>
2021-01-21 15:49:21 -0800

    commit

--- assignment1/Part 4/command.cpp
+++ assignment1/Part 4/command.cpp
@@ -6,18 +6,31 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    char arr[argc + 1]; 
+    char arr[sizeof(argv)]; 
 
     //print number of arguments(argc)
     cout << argc << endl;
-    int size = sizeof(argv);
+    
+    int size = sizeof(argc);
+    int m = size;
+   //cout << "Size:" << size << endl;
+
+    do{
+        cout << m << endl;
+        cout << argv[m];
+        m--;
+    }while(m =! 0);
+
 
     for(int i = 1; i <= argc; i++)
     {
-        cout << argv[i] << " ";//<< endl;
+    
+        //cout << argv[i] << " ";//<< endl;
+        
     }
-    cout << endl;
+    cout << endl << size;
 
+    
 
     /*while(size > 0){
         cout << argv << endl;
