dd92565088aa8873118ab2cb0f291833e149aa64
btphill4 <btphill4@asu.edu>
2021-01-24 00:18:03 -0800

    Finished Assignment 1

--- assignment1/Part 4/command.cpp
+++ assignment1/Part 4/command.cpp
@@ -6,24 +6,23 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    //print number of arguments(argc) with less than 
+    //print number of arguments(argc) with less than
     cout << argc - 1 << endl;
 
     //converts argv[] into a string pointer array
     string *temp = new string[argc];
+
+    string a = " ";
     for(int i = 0; i < argc; i++)
     {
-        temp[i] = argv[i];
+        temp[i] = argv[i] + a;
     }
 
     //prints the array
     for(int i = argc; i >= 1; i--)
     {
-        cout << temp[i] << " ";
+        cout << temp[i];
     }
-
-   //cout << "Size:" << size << endl;
-    
 
    return 0;
 }
