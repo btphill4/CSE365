4af91c24433457540840b013c8b7e09c407e29f7
btphill4 <btphill4@asu.edu>
2021-01-27 16:56:07 -0800

    Part 2 methods

--- assignment2/part2/secure_house.cpp
+++ assignment2/part2/secure_house.cpp
@@ -1,8 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <vector> 
 
 using namespace std;
+
+
+/** Methods
+ * insertKey()
+ * turnKey()
+ * enter()
+ * hasKey()
+ * FIREFIGHTER_SECRET_KEY
+ * whoseInside()
+ * peopleInHouse()
+*/
+
+//Variables
+string key; 
+string person;
+vector<string> house;
+string pKey;
+
+//Key Methods
+string returnKey(){
+    return key;
+}
+string insertKey(){
+
+}
+
+string turnKey(){
+    if(pKey == key)
+    {
+        cout << "Success " << person << "TURNS KEY" << key;
+    }
+}
+
+string enter()
+{
+    if(canEnter() == true)
+    {
+        house.push_back(person);
+    }
+    else
+    {
+       cout << "ACCESS DENIED" << endl;
+    }
+}
+bool canEnter(){
+
+}
+
+string whoseInside()
+{
+    if (house.empty())
+    {
+        cout << "Empty";
+    } 
+    else 
+        for(int i = 0; i < house.size(); i++)
+        {
+            cout << house.at(i);
+        }
+}
+
 
 int main()
 {
