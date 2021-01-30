7388af0d98b478f8dba214200bc578716fe5e110
btphill4 <btphill4@asu.edu>
2021-01-29 23:12:26 -0800

    Updated changes

--- assignment2/part2/secure_house.cpp
+++ assignment2/part2/secure_house.cpp
@@ -26,28 +26,57 @@ WHO'S INSIDE?
  * peopleInHouse()
 */
 
+/** 
+Given the input:
+INSERT KEY adam key
+TURN KEY adam
+ENTER HOUSE adam
+INSERT KEY pat foobar
+TURN KEY pat
+ENTER HOUSE pat
+WHO'S INSIDE?
+
+Given output: 
+KEY key INSERTED BY adam
+FAILURE adam UNABLE TO TURN KEY key
+ACCESS DENIED
+KEY foobar INSERTED BY pat
+SUCCESS pat TURNS KEY foobar
+ACCESS ALLOWED
+pat
+*/
+
+
 //Variables
-string key; 
+string* key = new string[200]; 
 string person;
 vector<string> house;
 string pKey;
 
 //Key Methods
-string returnKey(){
-    return key;
+string returnKeys(){
+    for(int i = 0; i <= sizeof(key); i++){
+        return key[i];
+    }
 }
 string insertKey(){
-
+    return "Key Inserted";
 }
 
 string turnKey(){
-    if(pKey == key)
-    {
-        cout << "Success " << person << "TURNS KEY" << key;
+    for(int i = 0; i < sizeof(key);i++){
+        if(pKey == key[i])
+        {
+            cout << "Success " << person << "TURNS KEY" << key;
+        }
     }
 }
 
-string enter()
+/*bool canEnter(){
+
+}*/
+
+/*string enter()
 {
     if(canEnter() == true)
     {
@@ -57,28 +86,35 @@ string enter()
     {
        cout << "ACCESS DENIED" << endl;
     }
-}
-bool canEnter(){
-
-}
+} */
 
 string whoseInside()
 {
     if (house.empty())
     {
-        cout << "Empty";
+        return "NOBODY HOME";
     } 
     else 
         for(int i = 0; i < house.size(); i++)
         {
-            cout << house.at(i);
+            return house.at(i);
         }
 }
 
 
 int main(int argc, char* argv[])
 {
- 
-    
-    
+    int size = sizeof(argv);
+    string fireFighterKey = "FIREFIGHTER_SECRET_KEY";
+
+    for(int i = 2; i <= size; i++){
+    key[i] = argv[2];
+    cout << key;
+    }
+
+    fireFighterKey = key[sizeof(key) + 1];
+
+
+
+
 }
